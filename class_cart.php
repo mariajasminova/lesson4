f98cb97b6a90ef9c503ad01e3cd643a624f04659
mariajasminova <masha.petrashova@yandex.ru>
2018-10-28 04:07:33 +0300

    Added functions for removing and adding discounts

--- class_cart.php
+++ class_cart.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
 * 
 */
@@ -10,7 +10,7 @@ class Cart
 		# code...
 	}
 
-	// Добавляет продукт в количестве count с указанным id в корзину
+// Добавляет продукт в количестве count с указанным id в корзину
 	public static function addProducrt($id_product, $count = 1)
 	{
 		// Соединение с БД
@@ -37,34 +37,75 @@ class Cart
 	public static function getProductsCartList()
 	{
 		// Соединение с БД
-   	$db = Db::getConnection();
-  	// Запрос к БД и получение результата
-   	$query = "SELECT `id_cart`, `name`, `count` FROM `product`, `cart` WHERE `cart`.`id_product_cart` = `product`.`id_product`";
-  	$result = mysqli_query($link, $query);
-  	//массив с информауцие о товаре в корзине
-  	$products_cart_list = array();
-  	$i = 0;
-  	//заполнение массива
-  	while ($row = mysqli_fetch_assoc($result)) {
-      $products_cart_list[$i]['id_cart'] = $row['id_cart'];
-      $products_cart_list[$i]['name'] = $row['name'];
-      $products_cart_list[$i]['count'] = $row['count'];
-
-      $i++;
-    }
-    return $products_cart_list;
+		$db = Db::getConnection();
+  		// Запрос к БД и получение результата
+   		$query = "SELECT id_cart, name, count, price, discount_price FROM product, cart WHERE `cart`.`id_product_cart` = `product`.`id_product`";
+  		$result = mysqli_query($db, $query);
+  		//массив с информауцие о товаре в корзине
+  		$products_cart_list = array();
+  		$i = 0;
+  		//заполнение массива
+  		while ($row = mysqli_fetch_assoc($result)) {
+			$products_cart_list[$i]['id_cart'] = $row['id_cart'];
+			$products_cart_list[$i]['name'] = $row['name'];
+			$products_cart_list[$i]['count'] = $row['count'];
+			$products_cart_list[$i]['price'] = $row['count'];
+			$products_cart_list[$i]['discount_price'] = $row['discount_price'];
+      
+     			$i++;
+    		}
+   		return $products_cart_list;
 	}
 
 	// Удаляет продукт из корзины с указанным id (id записи в таблице cart)
 	public static function deleteProductsCart($id_cart)
 	{
 		// Соединение с БД
-   	$db = Db::getConnection();
-   	// Запрос к БД и получение результата
-	  $query = "DELETE FROM cart WHERE id_cart = ".$id_cart;
-	  $result = mysqli_query($link, $query);
+   		$db = Db::getConnection();
+   		// Запрос к БД и получение результата
+	  	$query = "DELETE FROM cart WHERE id_cart = ".$id_cart;
+	 	$result = mysqli_query($db, $query);
 
-	  return $result; // boolean
+	 	return $result; // boolean
+	}
+
+	// Возвращает количество товаров в корзине 
+	public static function getCountProductsCart()
+	{
+		// Соединение с БД
+   		$db = Db::getConnection();
+  		// Запрос к БД и получение результата
+   		$query = "SELECT COUNT(id_cart) FROM cart";
+  		$result = mysqli_query($db, $query);
+  		//заполнение массива
+  		return mysqli_fetch_assoc($result);
+	}
+
+
+	// Возвращает сумму товаров в корзине без учета скидок 
+	public static function getFullPriceWithoutDiscounts()
+	{
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "SELECT SUM(`product`.`price`*`cart`.`count`) FROM `product`, `cart` WHERE `cart`.`id_product_cart`=`product`.`id_product`";
+		$result = mysqli_query($db, $query);
+		$price_without_discount = mysqli_fetch_assoc($result);
+		
+		return $price_without_discount["SUM(`product`.`price`*`cart`.`count`)"];        
+	}
+
+	// Возвращает сумму товаров в корзине с учетом скидок 
+	public static function getFullDiscountPrice()
+	{
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "SELECT SUM(`product`.`discount_price`*`cart`.`count`) FROM `product`, `cart` WHERE `cart`.`id_product_cart`=`product`.`id_product`";
+		$result = mysqli_query($db, $query);
+		$price_without_discount = mysqli_fetch_assoc($result);
+
+		return $price_without_discount["SUM(`product`.`price`*`cart`.`count`)"];        
 	}
 
 
@@ -72,3 +113,5 @@ class Cart
 }
 
 ?>
+
+
--- class_product.php
+++ class_product.php
@@ -1,137 +1,115 @@
-<?php
+﻿<?php
 
 /**
 * 
 */
 class Product
 {
-	 // Количество отображаемых товаров по умолчанию
-    const SHOW_BY_DEFAULT = 6;
 	
 	function __construct(argument)
 	{
 		# code...
 	}
 
-
-	//Возвращает список последних продуктов
-	public static function getLatestProducts($count = self::SHOW_BY_DEFAULT)
+	// Возвращает список товаров
+	public static function getProductsList()
 	{
 		// Соединение с БД
-		$db = Db::getConnection();
+	   	$db = Db::getConnection();
 
-		//запрос к БД и получение результата
-		$query = "SELECT * FROM product ORDER BY id_product DESC LIMIT ".$count;
-		$result = mysqli_query($link, $query);
+		// Запрос к БД и получение результата
+	  	$query = "SELECT * FROM product ORDER BY id_product ASC";
+	  	$result = mysqli_query($db, $query);
 
-		if (mysqli_num_rows($result) > 0) {
+ 	 	//массив с информауцие о товаре
+	  	$products_list = array();
+	  	$i = 0;
+	  	//заполнение массива
+  		while ($row = mysqli_fetch_assoc($result)) {
+			$products_list[$i]['id'] = $row['id'];
+			$products_list[$i]['name'] = $row['name'];
+			$products_list[$i]['price'] = $row['price'];
+			$products_list[$i]['current_discount'] = $row['current_discount'];
+			$products_list[$i]['sell_out'] = $row['sell_out'];
+			$products_list[$i]['full_discount'] = $row['full_discount'];
+			$products_list[$i]['discount_price'] = $row['discount_price'];
 
-		$row = mysqli_fetch_array($result);
-
-		//отборажение списка товаров (название товара + кнопка "в корзину")
-		do {
-			
-			echo '
-			<form action="class_cart.php" method="post">
-			<span>'. $row["name"].'</span>
-			<input type="hidden" name="id" value="'.$row["id_product"].'">
-			<input type="submit" value="в корзину" /> </br> 
-			</form>
-			';
-
-			} while ($row = mysqli_fetch_array($result));
-
-		}
-	  // очищение результирующего набора 
-	  mysqli_free_result($result);
-		// закрытие подключения 
-		mysqli_close($db);
+			$i++;
+    		}
+		return $products_list;
 	}
-
-  //Возвращает список товаров
-  public static function getProductsList()
-  {
-    // Соединение с БД
-   	$db = Db::getConnection();
-
-   // Запрос к БД и получение результата
-  	$query = "SELECT * FROM product ORDER BY id_product ASC";
-  	$result = mysqli_query($link, $query);
-
-  	//массив с информауцие о товаре
-  	$productsList = array();
-  	$i = 0;
-  	//заполнение массива
-  	while ($row = mysqli_fetch_assoc($result)) {
-      $productsList[$i]['id'] = $row['id'];
-      $productsList[$i]['name'] = $row['name'];
-      $productsList[$i]['price'] = $row['price'];
-      $productsList[$i]['current_discount'] = $row['current_discount'];
-
-      $i++;
-    }
-    return $productsList;
-  }
 
 	// Возвращает продукт с указанным id
 	public static function getProductById($id_product)
 	{
-	  // Соединение с БД
-	  $db = Db::getConnection();
-	  // Запрос к БД и получение результата
-	  $query = "SELECT * FROM product WHERE id_product = ".$id_product;
-	  $result = mysqli_query($link, $query);
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "SELECT * FROM product WHERE id_product = ".$id_product;
+		 $result = mysqli_query($db, $query);
 			        
-	  return mysqli_fetch_assoc($result);
+		return mysqli_fetch_assoc($result);
 	}
 
 	// Удаляет продук с указанным id
 	public static function deleteProductById($id_product)
 	{
-	  // Соединение с БД
-	  $db = Db::getConnection();
-	  // Запрос к БД и получение результата
-	  $query = "DELETE FROM product WHERE id_product = ".$id_product;
-	  $result = mysqli_query($link, $query);
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "DELETE FROM product WHERE id_product = ".$id_product;
+		$result = mysqli_query($db, $query);
 
-	  return $result; // boolean
+		return $result; // boolean
 	}
 
 	// Устанавливает скидку на все товары (распродажа)
 	public static function setSellOut($sell_out)
 	{
-	  // Соединение с БД
-	  $db = Db::getConnection();
-	  // Запрос к БД и получение результата
-	  $query = "UPDATE `product` SET `sell_out`= ".$sell_out;
-	  $result = mysqli_query($db, $query);
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "UPDATE product SET sell_out= ".$sell_out;
+		$result = mysqli_query($db, $query);
 			        
-	  return $result; // boolean
+		return $result; // boolean
 	}
 
 
 	// Устанавливает текущую скидку на продук с указанным id 
 	public static function setCurrentDiscount($current_discount, $id_product)
 	{
-	  // Соединение с БД
-	  $db = Db::getConnection();
-	  // Запрос к БД и получение результата
-	  $query = "UPDATE `product` SET `current_discount`= ".$current_discount." WHERE `id_product`= ".$id_product;
-	  $result = mysqli_query($db, $query);
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "UPDATE product SET current_discount= ".$current_discount." WHERE id_product= ".$id_product;
+		$result = mysqli_query($db, $query);
 			        
-	  return $result; // boolean
+		return $result; // boolean
 	}
 
-//Устанавливает скидку покупателя 
+	// Устанавливает скидку покупателя 
 	public static function setBuyerDiscount($buyer_discount, $id_customer)
 	{
-	  // Соединение с БД
-	  $db = Db::getConnection();
-	  // Запрос к БД и получение результата
-	  $query = "UPDATE `customer` SET `current_discount`= ".$buyer_discount." WHERE `id_customer`= ".$id_customer;
-	  $result = mysqli_query($db, $query);
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата	
+		$query = "UPDATE customer SET current_discount= ".$buyer_discount." WHERE id_customer= ".$id_customer;
+		$result = mysqli_query($db, $query);
 			        
-	  return $result; // boolean
+		return $result; // boolean
+	}
+
+	//Возвращает скидку покупателя
+	public static function getBuyerDiscount($id_customer)
+	{
+		// Соединение с БД
+		$db = Db::getConnection();
+		// Запрос к БД и получение результата
+		$query = "SELECT * FROM customer WHERE id_customer = ".$id_customer;
+		$result = mysqli_query($db, $query);
+			        
+		return mysqli_fetch_assoc($result);
 	}
 
 
